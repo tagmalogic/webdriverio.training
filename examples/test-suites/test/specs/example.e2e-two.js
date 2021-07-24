6c49ae9f4c77aec32de067b2f65f2b80aee10f29
mihaiwww <wwwvilcu@gmail.com>
2021-07-24 06:58:47 +0100

    upd

---
+++ examples/test-suites/test/specs/example.e2e-two.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const expectedPageTitle = "Dashboard ‹ wdiotraining — WordPress";
+
+describe("Login to wordpress", () => {
+  it("Should login with valid credentials", async () => {
+    await browser.url(`wp-login.php`);
+    await browser.pause(2000);
+
+    const userName = await browser.$("#user_login");
+    await userName.setValue("wdiotraining");
+
+    const userPass = await browser.$("#user_pass");
+    await userPass.setValue("wdiopass");
+    await browser.pause(2000);
+
+    const submitBtn = await browser.$("#wp-submit");
+    await submitBtn.click();
+
+    await browser.pause(2000);
+    const pageTitle = await browser.getTitle();
+
+    assert.strict.equal(pageTitle, expectedPageTitle);
+
+    await browser.pause(2000);
+  });
+});
